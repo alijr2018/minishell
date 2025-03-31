88d564682b44c0949ab15ef46fc8fe6f4a254488
alijr2018 <alijr_2018@proton.me>
2025-03-31 02:07:40 -0700

    still problem with echo && | and << >> < >

--- exec.c
+++ exec.c
@@ -6,7 +6,7 @@
 /*   By: abrami <abrami@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 10:51:01 by abrami            #+#    #+#             */
-/*   Updated: 2025/03/25 11:13:13 by abrami           ###   ########.fr       */
+/*   Updated: 2025/03/31 02:06:45 by abrami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static char	*ft_return(char *full_path, char *dir, char **paths, char *str)
 	i = 0;
 	while (paths[i])
 	{
-		full_path = malloc(strlen(paths[i]) + strlen(str) + 2);
+		full_path = malloc(ft_strlen(paths[i]) + ft_strlen(str) + 2);
 		if (!full_path)
 		{
 			i++;
@@ -70,13 +70,133 @@ static char	*searchexec(char *str)
 	ft_return(full_path, dir, paths, str);
 }
 
+void	command_cd(char **alt)
+{
+	char	*path;
+
+	if (!alt[1])
+	{
+		path = getenv("HOME");
+		if (!path)
+			return ;
+	}
+	else
+		path = alt[1];
+	if (chdir(path) != 0)
+		perror("cd");
+}
+
+int handle_options(char **args, int *i, int *no_newline, int *interpret_backslashes)
+{
+	while (args[*i] && args[*i][0] == '-' && args[*i][1] != '\0')
+	{
+		if (ft_strcmp(args[*i], "-n") == 0)
+			*no_newline = 1;
+		else if (ft_strcmp(args[*i], "-e") == 0)
+			*interpret_backslashes = 1;
+		else if (ft_strcmp(args[*i], "-E") == 0)
+			*interpret_backslashes = 0;
+		else
+			break;
+		(*i)++;
+	}
+	return (*i);
+}
+
+char *remove_quotes(char *arg)
+{
+	int	len;
+	
+	len = ft_strlen(arg);
+	if ((arg[0] == '"' && arg[len - 1] == '"') || (arg[0] == '\'' && arg[len - 1] == '\''))
+	{
+		arg[len - 1] = '\0';
+		arg++;
+	}
+	if (*arg == '\0')
+		return (arg);
+	while (*arg && isspace((unsigned char)*arg)) 
+		arg++;
+	return (arg);
+}
+
+char *process_escape_sequences(char *arg, int *interpret_backslashes)
+{
+	char	*new_arg;
+	int		j;
+	int		k;
+
+	if (*interpret_backslashes)
+	{
+		new_arg = malloc(ft_strlen(arg) + 1);
+		j = 0;
+		k = 0;
+		while (arg[k] != '\0')
+		{
+			if (arg[k] == '\\' && arg[k + 1] != '\0')
+			{
+				k++;
+				if (arg[k] == 'n')
+					new_arg[j++] = '\n';
+				else if (arg[k] == 't')
+					new_arg[j++] = '\t';
+				else if (arg[k] == '\\')
+					new_arg[j++] = '\\';
+				else
+					new_arg[j++] = arg[k];
+			}
+			else
+				new_arg[j++] = arg[k];
+			k++;
+		}
+		new_arg[j] = '\0';
+	    return (new_arg);
+    }
+    return (arg);
+}
+
+void print_arguments(char **args, int i, int interpret_backslashes)
+{
+	char	*arg;
+	char	*processed_arg;
+
+    while (args[i])
+    {
+        arg = args[i];
+        arg = remove_quotes(arg);
+		processed_arg = process_escape_sequences(arg, &interpret_backslashes);
+        ft_printf("%s", processed_arg);
+        if (args[i + 1])
+            ft_printf(" ");
+        i++;
+    }
+}
+
+void ft_echo(char **args)
+{
+    int	i;
+    int	no_newline;
+    int	interpret_backslashes;
+
+	i = 1;
+	no_newline = 0;
+    interpret_backslashes = 0;
+    i = handle_options(args, &i, &no_newline, &interpret_backslashes);
+    print_arguments(args, i, interpret_backslashes);
+    if (!no_newline)
+        ft_printf("\n");
+}
 void	exec(char **alt)
 {
 	char	*exec_path;
 
 	if (ft_strcmp(*alt, "cd") == 0)
+		return (command_cd(alt));
+	if (ft_strcmp(*alt, "echo") == 0)
+		return (ft_echo(alt));
+	if (ft_strcmp(*alt, "$?") == 0)
 	{
-		ft_printf("hello");
+		ft_printf("0: command not found\n");
 		return ;
 	}
 	exec_path = searchexec(*alt);
--- minishell.c
+++ minishell.c
@@ -12,19 +12,6 @@
 
 #include "minishell.h"
 
-// static int	count_word(char **s)
-// {
-// 	int	count;
-
-// 	count = 0;
-// 	while (*s)
-// 	{
-//         s++;
-//         count++;
-// 	}
-// 	return (count);
-// }
-
 int	ft_strcmp(char *srt, char *str)
 {
 	int	i;
@@ -41,27 +28,151 @@ static void	ft_exit(char *input)
 	free(input);
 }
 
-static void	ft_run(char **search)
+int has_unclosed_quotes(const char *str)
 {
-	pid_t	pid;
+    int	single_quote;
+	int	double_quote;
 
-	pid = fork();
-	if (pid == 0)
-		exec(search);
-	else if (pid > 0)
-		wait (0);
-	else
-		write (2, "Error\n", 6);
+    single_quote = 0;
+	double_quote = 0;
+	while (*str)
+    {
+        if (*str == '\'' && double_quote == 0)
+            single_quote = !single_quote;
+        else if (*str == '"' && single_quote == 0)
+		double_quote = !double_quote;
+	str++;
+    }
+    return (single_quote || double_quote);
+	// Return 1 if there's an unclosed quote
 }
+
+char *read_full_command(void)
+{
+    char	*input;
+    char	*temp;
+    char	*line;
+
+    input = NULL;
+	input = readline("minishell!> ");
+    if (!input)
+		return (NULL);
+	while (has_unclosed_quotes(input))
+    {
+        temp = readline("> ");
+        if (!temp)
+            break;
+        line = malloc(ft_strlen(input) + ft_strlen(temp) + 2);
+        if (!line)
+            return (input);
+		strcpy(line, input);
+		strcat(line, "/");
+		strcat(line, temp);
+        free(input);
+        input = line;
+        free(temp);
+    }
+    return (input);
+}
+static void ft_run(char **search)
+{
+    pid_t pid;
+    pid = fork();
+
+    if (pid == 0)
+    {
+        exec(search); // Execute the command
+        // perror("execve failed");
+        // exit(1);
+    }
+    else if (pid > 0)
+    {
+        wait(NULL); // Wait for child process to finish
+    }
+    else
+    {
+        perror("Fork failed");
+    }
+}
+
+void handle_pipes(char **args) {
+    int i = 0;
+    int pipe_fd[2];
+    pid_t pid;
+    int prev_pipe = -1; // This variable stores the previous pipe's read end
+
+    while (args[i]) {
+        // When we encounter the pipe
+        if (strcmp(args[i], "|") == 0) {
+            // Create a pipe
+            if (pipe(pipe_fd) == -1) {
+                perror("Pipe failed");
+                exit(1);
+            }
+
+            pid = fork();
+            if (pid == 0) {
+                // Child process
+                if (prev_pipe != -1) {
+                    // Redirect the previous pipe's read end to stdin
+                    dup2(prev_pipe, STDIN_FILENO);
+                    close(prev_pipe);
+                }
+                close(pipe_fd[0]); // Close read end of current pipe
+                dup2(pipe_fd[1], STDOUT_FILENO); // Redirect stdout to pipe
+                args[i] = NULL; // Terminate the current command at the pipe
+                ft_run(args); // Execute the command
+            } else if (pid > 0) {
+                // Parent process
+                close(pipe_fd[1]); // Close write end of current pipe
+                if (prev_pipe != -1) {
+                    close(prev_pipe); // Close previous pipe's read end
+                }
+                prev_pipe = pipe_fd[0]; // Update the previous pipe's read end
+                i++; // Move to the next command after the pipe
+            } else {
+                perror("Fork failed");
+                exit(1);
+            }
+        } else {
+            i++; // Keep moving until we find the pipe or end of commands
+        }
+    }
+
+    // If no pipe was encountered, execute the last command (if any)
+    if (prev_pipe != -1) {
+        close(prev_pipe);
+    }
+
+    // Wait for all child processes to finish
+    while (wait(NULL) > 0);
+}
+
+
+void handle_redirections_and_pipes(char **args)
+{
+    int i = 0;
+
+    // Check for any pipes in the command
+    handle_pipes(args);
+
+    // After handling pipes, apply any redirections and run the command
+    // handle_redirections(args);
+
+    // If no pipe was present, execute the command normally
+    ft_run(args);
+}
+
+
 
 int	main(void)
 {
 	char	*input;
 	char	**search;
-
+	
 	while (1)
 	{
-		input = readline("minishell! ");
+		input = read_full_command();
 		if (!input)
 		{
 			ft_exit(input);
@@ -75,7 +186,8 @@ int	main(void)
 		if (*input)
 			add_history(input);
 		search = ft_split(input, ' ');
-		ft_run(search);
+		handle_redirections_and_pipes(search);
+		// ft_run(search);
 		free(input);
 	}
 	rl_clear_history();
